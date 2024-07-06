870641830d42fb00ba372eb9acb89691aeee001e
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-07-06 23:24:48 +0300

    worked

--- src/controllers/check-ins.js
+++ src/controllers/check-ins.js
@@ -6,6 +6,7 @@ import {
 } from '../services/check-ins.js';
 
 import { createClient } from '../services/clients.js';
+import { updateRoom, getRoomById } from '../services/rooms.js';
 
 export const getAllCheckInsController = async (req, res) => {
   const checkIns = await getAllCheckIns();
@@ -28,15 +29,24 @@ export const getCheckInByIdController = async (req, res) => {
 };
 
 export const createCheckInClientController = async (req, res) => {
-
-  const { last_name, first_name, middle_name, passport_details, room, check_in_date, check_out_date, comment, note } = req.body;
+  const {
+    last_name,
+    first_name,
+    middle_name,
+    passport_details,
+    room,
+    check_in_date,
+    check_out_date,
+    comment,
+    note,
+  } = req.body;
 
   const clientData = {
     last_name,
     first_name,
     middle_name,
     passport_details,
-     comment
+    comment,
   };
 
   const client = await createClient(clientData);
@@ -48,18 +58,26 @@ export const createCheckInClientController = async (req, res) => {
     client: clientId,
     check_in_date,
     check_out_date,
-    note
+    note,
   };
 
   const checkIn = await createCheckIn(checkInData);
 
+  const targetedRoom = await getRoomById(room);
+  const bookings = targetedRoom.bookings.push({
+    check_in_date,
+    check_out_date,
+  });
+  const updatedRoom = await updateRoom(room, bookings);
+
   res.status(201).json({
     status: 201,
-    message: `Successfully created client with check-in!`,
+    message: `Successfully created client with check-in and updated room!`,
     data: {
-    client: client,
-    checkIn: checkIn
-  }
+      client,
+      checkIn,
+      updatedRoom
+    },
   });
 };
 
