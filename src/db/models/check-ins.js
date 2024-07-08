f00730a2a93a4c3eff6077a0da4848cc17941dd3
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-07-08 19:20:29 +0300

    worked

--- src/db/models/check-ins.js
+++ src/db/models/check-ins.js
@@ -3,15 +3,14 @@ import { Schema, model } from 'mongoose';
 
 const checkInSchema = new Schema(
   {
-
     client: { type: Schema.Types.ObjectId, ref: 'client', required: true },
     room: { type: Schema.Types.ObjectId, ref: 'room', required: true },
     check_in_date: { type: Date, required: true },
     check_out_date: { type: Date, required: true },
     note: { type: String },
-
+    isCheckIn: { type: Boolean, required: true },
   },
-  { timestamps: false, versionKey: false }
+  { timestamps: false, versionKey: false },
 );
 
 export const CheckIn = model('check-in', checkInSchema);
