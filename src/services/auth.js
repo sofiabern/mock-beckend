b5596d4131430e7becae1dcabcd17cb9976e61bd
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-07-21 18:26:40 +0300

    worked

--- src/services/auth.js
+++ src/services/auth.js
@@ -39,7 +39,7 @@ export const loginUser = async ({ email, password }) => {
     id
   };
 
-  const token = jwt.sign(payload, JWT_SECRET, {expiresIn: "12h"});
+  const token = jwt.sign(payload, JWT_SECRET, {expiresIn: "3m"});
 await User.findByIdAndUpdate(id, {token});
   return token;
 };
